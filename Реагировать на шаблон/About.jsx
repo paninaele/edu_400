0282ff47f6114d5be65f5c5e5d00486a2576130f
paninaele <83338738+paninaele@users.noreply.github.com>
2021-05-23 21:03:23 +0300

    Create About.jsx

---
+++ Реагировать на шаблон/About.jsx
@@ -0,0 +1,11 @@
+export function About(){
+    return (
+        <div>
+            <h1>О нас</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+
+            </p>
+        </div>
+    )
+}
